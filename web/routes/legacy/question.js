e5d6f5c705669bc31ec5addf6c7a53424118fd5f
Fokke Zandbergen <mail@fokkezb.nl>
2016-01-18 14:03:29 +0100

    Uses legacy JSON files to get title of question for better search

--- web/routes/legacy/question.js
+++ web/routes/legacy/question.js
@@ -1,6 +1,9 @@
+var _ = require('lodash');
+
 var Arrow = require('arrow');
 
 var utils = require('../../../lib/utils');
+var qa = require('../../../lib/legacy/qa');
 
 module.exports = Arrow.Router.extend({
 	name: __filename,
@@ -11,23 +14,51 @@ module.exports = Arrow.Router.extend({
 	// /question/183184
 	// /question/
 	// /question
-	path: /^\/question(?:$|\/(?:[0-9]+\/([^\/]+))?)/,
+	path: /^\/question(?:$|\/([0-9]+)?(?:$|\/([^\/]+)?))/,
 
 	method: 'GET',
-	action: function (req, res) {
-		var slug = req.params['0'];
+	action: function (req, res, next) {
+		var id = parseInt(req.params['0']);
+		var slug = req.params['1'];
+
 		var opts = {};
 
-		if (slug) {
-			slug = slug.replace(/-/g, ' ');
+		if (id) {
 
-			opts.url = 'https://www.google.com/search?q=' + utils.encodeForURI('site:stackoverflow.com +appcelerator ' + slug);
-			opts.action = 'Try <a href="http://webcache.googleusercontent.com/search?q=cache:https://developer.appcelerator.com' + req.originalUrl + '&num=1&strip=1&vwsrc=0">Google\'s cache</a> or <a href="' + opts.url + '">Search Stack Overflow</a> for <em>' + slug + '</em>.';
+			qa.getQuestionById(id, function (err, question) {
+
+				if (err) {
+
+					if (slug) {
+						opts.title = slug.replace(/-/g, ' ');
+					}
+
+				} else {
+					opts.title = question.title;
+					// opts.question = qa.markup(question.question);
+					//
+					// if (question.accepted_answer_id) {
+					// 	opts.answer = _.find(question.answers, {
+					// 		id: question.accepted_answer_id
+					// 	});
+					// }
+				}
+
+				opts.url = 'https://www.google.com/search?q=' + utils.encodeForURI('site:stackoverflow.com +appcelerator ' + opts.title);
+				opts.action = '<a href="' + opts.url + '">Search Stack Overflow</a> for <em>"' + opts.title + '"</em> or try <a href="http://webcache.googleusercontent.com/search?q=cache:https://developer.appcelerator.com' + req.originalUrl + '&num=1&strip=1&vwsrc=0">Google\'s cache</a>.';
+
+				utils.redirect(req, res, opts);
+
+				return next();
+
+			});
 
 		} else {
 			opts.url = '/';
-		}
 
-		utils.redirect(req, res, opts);
+			utils.redirect(req, res, opts);
+
+			return next();
+		}
 	}
 });
