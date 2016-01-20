b0618dc6ac71c2a71fd189a149ba8677588538a6
Fokke Zandbergen <mail@fokkezb.nl>
2016-01-20 23:34:34 +0100

    redirect to archive

--- web/routes/legacy/question.js
+++ web/routes/legacy/question.js
@@ -1,9 +1,4 @@
-var _ = require('lodash');
-
 var Arrow = require('arrow');
-
-var utils = require('../../../lib/utils');
-var qa = require('../../../lib/legacy/qa');
 
 module.exports = Arrow.Router.extend({
 	name: __filename,
@@ -14,51 +9,17 @@ module.exports = Arrow.Router.extend({
 	// /question/183184
 	// /question/
 	// /question
-	path: /^\/question(?:$|\/([0-9]+)?(?:$|\/([^\/]+)?))/,
+	path: /^\/question(?:$|\/([0-9]+)?)/,
 
 	method: 'GET',
 	action: function (req, res, next) {
-		var id = parseInt(req.params['0']);
-		var slug = req.params['1'];
 
-		var opts = {};
-
-		if (id) {
-
-			qa.getQuestionById(id, function (err, question) {
-
-				if (err) {
-
-					if (slug) {
-						opts.title = slug.replace(/-/g, ' ');
-					}
-
-				} else {
-					opts.title = question.title;
-					// opts.question = qa.markup(question.question);
-					//
-					// if (question.accepted_answer_id) {
-					// 	opts.answer = _.find(question.answers, {
-					// 		id: question.accepted_answer_id
-					// 	});
-					// }
-				}
-
-				opts.url = 'https://www.google.com/search?q=' + utils.encodeForURI('site:stackoverflow.com +appcelerator ' + opts.title);
-				opts.action = '<a href="' + opts.url + '">Search Stack Overflow</a> for <em>"' + opts.title + '"</em> or try <a href="http://webcache.googleusercontent.com/search?q=cache:https://developer.appcelerator.com' + req.originalUrl + '&num=1&strip=1&vwsrc=0">Google\'s cache</a>.';
-
-				utils.redirect(req, res, opts);
-
-				return next();
-
-			});
-
+		if (req.params['0']) {
+			url = 'https://archive.appcelerator.com' + req.originalUrl;
 		} else {
-			opts.url = '/';
-
-			utils.redirect(req, res, opts);
-
-			return next();
+			url = '/help';
 		}
+
+		return res.redirect(301, url);
 	}
 });
