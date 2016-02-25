1fc024ba58b4d3be4a9288558c418e89c3c8268c
Fokke Zandbergen <mail@fokkezb.nl>
2016-02-25 21:56:54 +0100

    finished frontpage

--- conf/app.js
+++ conf/app.js
@@ -1,3 +1,154 @@
 module.exports = {
-	app: {}
+	app: {
+		index: {
+			developers: [{
+					username: 'RicardoAlcocer',
+					name: 'Ricardo Alcocer',
+					title: 'Director DevRel & Training'
+				}, {
+					username: 'FokkeZB',
+					name: 'Fokke Zandbergen',
+					title: 'Developer Evangelist'
+				}, {
+					username: 'JHaynie',
+					name: 'Jeff Haynie',
+					title: 'Co-Founder & CEO'
+				}, {
+					username: 'Hansemannnn',
+					name: 'Hans Knoechel',
+					title: 'iOS Engineer'
+				}, {
+					username: 'RBlalock',
+					name: 'Rick Blalock',
+					title: 'Mobile Architect'
+				}, {
+					username: 'AppcDev',
+					name: 'Appcelerator Dev',
+					title: 'Developer Relations'
+				}
+				/*, {
+								username: 'Appcelerator',
+								name: 'Appcelerator',
+								title: 'Corporate Account'
+							}*/
+			],
+			footerTop: [{
+				title: 'Product',
+				links: [{
+					title: 'Appcelerator Platform',
+					href: 'http://www.appcelerator.com/mobile-app-development-products/',
+					external: true
+				}, {
+					title: 'Plans &amp; Pricing',
+					href: 'http://www.appcelerator.com/pricing/',
+					external: true
+				}]
+			}, {
+				title: 'Developers',
+				links: [{
+					title: 'App U.',
+					href: 'http://university.appcelerator.com/',
+					external: true
+				}, {
+					title: 'Q&amp;A Support',
+					href: '/help'
+				}, {
+					title: 'Documentation',
+					href: 'http://docs.appcelerator.com/',
+					external: true
+				}, {
+					title: 'Events',
+					href: 'http://www.appcelerator.com/company/events/',
+					external: true
+				}]
+			}, {
+				title: 'Enterprise',
+				links: [{
+					title: 'Overview',
+					href: 'http://www.appcelerator.com/enterprise/',
+					external: true
+				}]
+			}, {
+				title: 'About',
+				links: [{
+					title: 'Company',
+					href: 'http://www.appcelerator.com/company/',
+					external: true
+				}, {
+					title: 'Jobs',
+					href: 'http://www.appcelerator.com/company/jobs/',
+					external: true
+				}, {
+					title: 'Leadership',
+					href: 'http://www.appcelerator.com/company/#leadership-team',
+					external: true
+				}, {
+					title: 'Contact Us',
+					href: 'http://www.appcelerator.com/contact-us/',
+					external: true
+				}, {
+					title: 'Partners',
+					href: 'http://www.appcelerator.com/partners/',
+					external: true
+				}]
+			}],
+			footerBottom: [{
+				title: 'Customers',
+				links: [{
+					title: 'Case Studies',
+					href: 'http://www.appcelerator.com/resource-center/case-studies/',
+					external: true
+				}, {
+					title: 'App Showcase',
+					href: 'http://www.appcelerator.com/company/app-showcase/',
+					external: true
+				}]
+			}, {
+				title: 'Media',
+				links: [{
+					title: 'Press',
+					href: 'http://www.appcelerator.com/company/news/',
+					external: true
+				}, {
+					title: 'Whitepapers',
+					href: 'http://www.appcelerator.com/resource-center/white-papers/',
+					external: true
+				}, {
+					title: 'Webinars',
+					href: 'http://www.appcelerator.com/resource-center/webinars/',
+					external: true
+				}, {
+					title: 'Research',
+					href: 'http://www.appcelerator.com/resource-center/research/',
+					external: true
+				}, {
+					title: 'Videos',
+					href: 'http://www.appcelerator.com/resource-center/videos/',
+					external: true
+				}]
+			}, {
+				title: 'Blog',
+				links: [{
+					title: 'Developer',
+					href: 'http://www.appcelerator.com/cat/developer/',
+					external: true
+				}, {
+					title: 'Enterprise',
+					href: 'http://www.appcelerator.com/cat/enterprise/',
+					external: true
+				}, {
+					title: 'Product Updates',
+					href: 'http://www.appcelerator.com/cat/product-updates/',
+					external: true
+				}]
+			}, {
+				title: 'Open Source',
+				links: [{
+					title: 'Titanium',
+					href: 'http://www.appcelerator.org/',
+					external: true
+				}]
+			}]
+		}
+	}
 };
--- web/routes/index.js
+++ web/routes/index.js
@@ -1,138 +1,151 @@
+var _ = require('lodash');
+var async = require('async');
 var Arrow = require('arrow');
+var helpers = require('../../lib/helpers');
+var moment = require('moment');
+var utils = require('../../lib/utils');
+
+var cache;
+var cachedAt;
 
 module.exports = Arrow.Router.extend({
 	name: __filename,
 	path: '/',
 	method: 'GET',
-	action: function (req, res) {
-		res.render('index', {
-			scripts: ['/js/crazyColors.js'],
-			developers: [{
-					username: 'RicardoAlcocer',
-					name: 'Ricardo Alcocer',
-					title: 'Director DevRel & Training'
-				}, {
-					username: 'FokkeZB',
-					name: 'Fokke Zandbergen',
-					title: 'Developer Evangelist'
-				}, {
-					username: 'JHaynie',
-					name: 'Jeff Haynie',
-					title: 'Co-Founder & CEO'
-				}, {
-					username: 'Hansemannnn',
-					name: 'Hans Knoechel',
-					title: 'iOS Engineer'
-				}, {
-					username: 'RBlalock',
-					name: 'Rick Blalock',
-					title: 'Mobile Architect'
-				}, {
-					username: 'AppcDev',
-					name: 'Appcelerator Dev',
-					title: 'Developer Relations'
+	action: function (req, res, next) {
+		var rendered = false;
+
+		function render(context) {
+
+			if (!rendered) {
+				rendered = true;
+
+				context.scripts = ['/js/crazyColors.js'];
+
+				_.extend(context, req.server.config.app.index);
+				res.render('index', context);
+			}
+
+			next();
+		}
+
+		// even if it's expired, we won't wait for the new data
+		if (cache) {
+			return render(cache);
+		}
+
+		// time to request (new) data
+		if (!cache || (Date.now() - cachedAt > (1000 * 60 * 15))) {
+
+			async.parallel({
+
+				devblog: function (callback) {
+					req.server.getAPI('/api/feeds/rss').execute({
+						url: 'http://www.appcelerator.com/cat/developer/feed/'
+					}, function (err, results) {
+						callback(null, results ? results[results.key].slice(0, 4) : null);
+					});
+				},
+
+				updates: function (callback) {
+
+					async.parallel([
+						function so_questions(callback) {
+							req.server.getAPI('/api/feeds/so_questions').execute(function (err, results) {
+								callback(null, results ? {
+									title: results[results.key][0].title,
+									link: results[results.key][0].link,
+									published: results[results.key][0].creation_date,
+									author: 'Stack Overflow'
+								} : null);
+							});
+						},
+						function marketplace(callback) {
+							req.server.getAPI('/api/feeds/marketplace').execute(function (err, results) {
+								callback(null, results ? {
+									title: results[results.key][0].name,
+									link: results[results.key][0].url,
+									published: results[results.key][0].lastModified,
+									author: 'Appcelerator Marketplace'
+								} : null);
+							});
+						},
+						function university(callback) {
+							req.server.getAPI('/api/feeds/university').execute(function (err, results) {
+								callback(null, results ? {
+									title: results[results.key][0].title,
+									link: 'http://university.appcelerator.com/video/' + results[results.key][0].id + '/' + helpers.slug(results[results.key][0].title),
+									published: results[results.key][0].date,
+									author: 'Appcelerator University'
+								} : null);
+							});
+						},
+						function samples(callback) {
+							req.server.getAPI('/api/feeds/samples').execute(function (err, results) {
+								callback(null, results ? {
+									title: results[results.key][0].name,
+									link: results[results.key][0].location,
+									published: results[results.key][0].created_at,
+									author: 'Appcelerator Samples'
+								} : null);
+							});
+						},
+						function gittio(callback) {
+							req.server.getAPI('/api/feeds/rss').execute({
+								url: 'http://gitt.io/rss.xml'
+							}, function (err, results) {
+								callback(null, results ? {
+									title: results[results.key][0].title,
+									link: results[results.key][0].link,
+									published: results[results.key][0].published,
+									author: 'gitTio'
+								} : null);
+							});
+						},
+						function jira(callback) {
+							req.server.getAPI('/api/feeds/rss').execute({
+								url: 'https://jira.appcelerator.org/sr/jira.issueviews:searchrequest-rss/temp/SearchRequest.xml?jqlQuery=project+in+%28AC%2C+TC%29+AND+resolution+%3D+Unresolved+ORDER+BY+created+DESC%2C+updated+ASC%2C+priority+DESC&tempMax=1000'
+							}, function (err, results) {
+								callback(null, results ? {
+									title: results[results.key][0].title,
+									link: results[results.key][0].link,
+									published: results[results.key][0].published,
+									author: 'JIRA'
+								} : null);
+							});
+						},
+						function medium(callback) {
+							req.server.getAPI('/api/feeds/rss').execute({
+								url: 'https://medium.com/feed/all-titanium'
+							}, function (err, results) {
+								callback(null, results ? {
+									title: results[results.key][0].title,
+									link: results[results.key][0].link,
+									published: results[results.key][0].published,
+									author: 'gitTio'
+								} : null);
+							});
+						}
+
+					], function (err, results) {
+
+						if (results) {
+							results = _.sortBy(results, function (o) {
+								return moment(o.published).valueOf();
+							}).reverse().slice(0, 3);
+						}
+
+						callback(null, results);
+					});
+
 				}
-				/*, {
-								username: 'Appcelerator',
-								name: 'Appcelerator',
-								title: 'Corporate Account'
-							}*/
-			],
-			footerTop: [{
-				title: 'Product',
-				links: [{
-					title: 'Appcelerator Platform',
-					href: 'http://www.appcelerator.com/mobile-app-development-products/'
-				}, {
-					title: 'Plans &amp; Pricing',
-					href: 'http://www.appcelerator.com/pricing/'
-				}]
-			}, {
-				title: 'Developers',
-				links: [{
-					title: 'App U.',
-					href: 'http://university.appcelerator.com/'
-				}, {
-					title: 'Q&amp;A Support',
-					href: 'http://community.appcelerator.com/'
-				}, {
-					title: 'Documentation',
-					href: 'http://docs.appcelerator.com/'
-				}, {
-					title: 'Events',
-					href: 'http://www.appcelerator.com/company/events/'
-				}]
-			}, {
-				title: 'Enterprise',
-				links: [{
-					title: 'Overview',
-					href: 'http://www.appcelerator.com/enterprise/'
-				}]
-			}, {
-				title: 'About',
-				links: [{
-					title: 'Company',
-					href: 'http://www.appcelerator.com/company/'
-				}, {
-					title: 'Jobs',
-					href: 'http://www.appcelerator.com/company/jobs/'
-				}, {
-					title: 'Leadership',
-					href: 'http://www.appcelerator.com/company/#leadership-team'
-				}, {
-					title: 'Contact Us',
-					href: 'http://www.appcelerator.com/contact-us/'
-				}, {
-					title: 'Partners',
-					href: 'http://www.appcelerator.com/partners/'
-				}]
-			}],
-			footerBottom: [{
-				title: 'Customers',
-				links: [{
-					title: 'Case Studies',
-					href: 'http://www.appcelerator.com/resource-center/case-studies/'
-				}, {
-					title: 'App Showcase',
-					href: 'http://www.appcelerator.com/company/app-showcase/'
-				}]
-			}, {
-				title: 'Media',
-				links: [{
-					title: 'Press',
-					href: 'http://www.appcelerator.com/company/news/'
-				}, {
-					title: 'Whitepapers',
-					href: 'http://www.appcelerator.com/resource-center/white-papers/'
-				}, {
-					title: 'Webinars',
-					href: 'http://www.appcelerator.com/resource-center/webinars/'
-				}, {
-					title: 'Research',
-					href: 'http://www.appcelerator.com/resource-center/research/'
-				}, {
-					title: 'Videos',
-					href: 'http://www.appcelerator.com/resource-center/videos/'
-				}]
-			}, {
-				title: 'Blog',
-				links: [{
-					title: 'Developer',
-					href: 'http://www.appcelerator.com/cat/developer/'
-				}, {
-					title: 'Enterprise',
-					href: 'http://www.appcelerator.com/cat/enterprise/'
-				}, {
-					title: 'Product Updates',
-					href: 'http://www.appcelerator.com/cat/product-updates/'
-				}]
-			}, {
-				title: 'Open Source',
-				links: [{
-					title: 'Titanium',
-					href: 'http://www.appcelerator.org/'
-				}]
-			}]
-		});
+			}, function (err, results) {
+
+				cachedAt = Date.now();
+				cache = results;
+
+				render(results);
+			});
+		}
 	}
 });
