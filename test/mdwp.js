4d1d600c6e1a8647077359029425e6ad5e706dcb
Fokke Zandbergen <mail@fokkezb.nl>
2016-03-03 12:46:14 +0100

    Add script to convert MD to WP

---
+++ test/mdwp.js
@@ -0,0 +1,30 @@
+var fs = require('fs');
+var path = require('path');
+var should = require('should');
+var mdwp = require('../lib/mdwp');
+
+var input = fs.readFileSync(path.join(__dirname, 'fixtures', 'mdwp-input.md'), 'utf8');
+var output = fs.readFileSync(path.join(__dirname, 'fixtures', 'mdwp-output.html'), 'utf8');
+
+describe('mdwp', function () {
+
+	it('converts', function (done) {
+
+		var wordpress = mdwp.convert({
+			url: 'https://github.com/appcelerator-developer-relations/appc-sample-ti520/blob/master/docs/transitions.md',
+			markdown: input
+		});
+
+		should(wordpress.title).eql('Titanium 5.2.0: Android Activity &amp; Shared Element Transitions');
+
+		if (wordpress.html !== output) {
+			var err = new Error('expected output to match');
+			err.expected = output;
+			err.actual = wordpress.html;
+			throw err;
+		}
+
+		done();
+	});
+
+});
---
+++ web/routes/scripts/mdwp.js
@@ -0,0 +1,73 @@
+var Arrow = require('arrow');
+var mdwp = require('lib/mdwp.js');
+var request = require('request');
+
+module.exports = Arrow.Router.extend({
+	name: __filename,
+	path: '/scripts/mdwp',
+	method: 'GET',
+	action: function (req, res, next) {
+
+		if (!req.session.user || req.session.user.email.indexOf('@appcelerator.com') === -1) {
+			return this.unauthorized(next);
+		}
+
+		var url, markdown, title, html;
+
+		function respond() {
+
+			if (url && markdown) {
+
+				try {
+
+					var converted = mdwp.convert({
+						url: url,
+						markdown: markdown
+					});
+
+					title = converted.title;
+					html = converted.html;
+
+				} catch (e) {
+					return next(e);
+				}
+			}
+
+			res.render('scripts/mdwp', {
+				title: "Convert MarkDown READMEs to Wordpress Posts",
+				url: url,
+				markdown: markdown,
+				wpTitle: title,
+				html: html
+			});
+
+			next();
+		}
+
+		if (req.query.url) {
+			url = req.query.url;
+		}
+
+		if (req.query.markdown) {
+			markdown = req.query.markdown;
+
+			respond();
+
+		} else if (url.indexOf('/blob/') !== -1) {
+			request(url.replace('/blob/', '/raw/'), function (error, response, body) {
+
+				if (error) {
+					return next(error);
+				}
+
+				markdown = body;
+
+				respond();
+			});
+			
+		} else {
+			respond();
+		}
+
+	}
+});
