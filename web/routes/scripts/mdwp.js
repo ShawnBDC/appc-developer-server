cf8332272d85d656cecd8cc98708caf3bb1dec03
Fokke Zandbergen <mail@fokkezb.nl>
2016-03-16 12:48:35 +0100

    Support for private repos in MDWP

--- web/routes/scripts/mdwp.js
+++ web/routes/scripts/mdwp.js
@@ -54,20 +54,45 @@ module.exports = Arrow.Router.extend({
 
 			respond();
 
-		} else if (url && url.indexOf('/blob/') !== -1) {
-			request(url.replace('/blob/', '/raw/'), function (error, response, body) {
+		} else {
+
+			var match = url.match(/^https:\/\/github\.com\/([^\/]+)\/([^\/]+)\/blob\/([^\/]+)\/(.+)$/);
+
+			if (!match) {
+				return respond();
+			}
+
+			var apiUrl = 'https://api.github.com/repos/' + match[1] + '/' + match[2] + '/contents/' + match[4] + '?ref=' + match[3];
+
+			request({
+				url: apiUrl,
+				json: true,
+				auth: {
+					user: 'fokkezb',
+					pass: 'c7ae7f1f32da9d500d8991092de668305fb1e0ed'
+				},
+				headers: {
+					'User-Agent': 'Appcelerator Arrow'
+				}
+			}, function (error, response, body) {
 
 				if (error) {
 					return next(error);
 				}
 
-				markdown = body;
+				if (response.statusCode !== 200) {
+					return next(new HttpError(response.statusCode, body.message));
+				}
+
+				if (!body.content) {
+					return next(new HttpError(400, 'File not found on GitHub'));
+				}
+
+				markdown = new Buffer(body.content, 'base64').toString('ascii');
 
 				respond();
 			});
 
-		} else {
-			respond();
 		}
 
 	}
