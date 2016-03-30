69e9c82f5d77c3b3796e777803d575c5fac00f99
Fokke Zandbergen <mail@fokkezb.nl>
2016-03-30 13:20:30 +0200

    Add date filter on MBIP

--- web/routes/scripts/mbip.js
+++ web/routes/scripts/mbip.js
@@ -1,6 +1,7 @@
 var Arrow = require('arrow');
 var HttpError = require('lib/HttpError');
 var utils = require('lib/utils');
+var moment = require('moment');
 
 var URL = 'http://www.appcelerator.com/social-rss-feed/?numposts=30&cat=9%2C12';
 
@@ -14,19 +15,42 @@ module.exports = Arrow.Router.extend({
 			return next(new HttpError(401, 'Unauthorized'));
 		}
 
-		utils.readFeed(URL, function (err, items) {
+		if (req.query.from) {
 
-			if (!items) {
-				return next(err || 'Failed to fetch posts');
+			var from = moment(req.query.from);
+
+			if (!from.isValid()) {
+				return next('Not a valid date.');
 			}
+
+			utils.readFeed(URL, function (err, items) {
+
+				if (!items) {
+					return next(err || 'Failed to fetch posts');
+				}
+
+				var posts = items.filter(function (post) {
+					return moment(post.date).isSameOrAfter(from);
+				});
+
+				res.render('scripts/mbip', {
+					title: 'Mobile Impact',
+					posts: posts,
+					from: req.query.from
+				});
+
+				next();
+			});
+
+		} else {
 
 			res.render('scripts/mbip', {
 				title: 'Mobile Impact',
-				posts: items
+				from: moment().subtract(1, 'months').format('YYYY-MM-DD')
 			});
 
 			next();
-		});
+		}
 
 	}
 });
