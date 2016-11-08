c4f05dc29f7c1a5f23720db7a31780eb6a120c81
jasonkneen <jason@bouncingfish.com>
2016-11-08 18:06:00 +0000

    remove Jeff, Bert, add Jason and cleanup

--- conf/app.js
+++ conf/app.js
@@ -8,32 +8,15 @@ module.exports = {
 					username: 'AppcDev',
 					name: 'Appcelerator Dev',
 					title: 'Developer Relations'
-				// }, {
-				// 	username: 'RicardoAlcocer',
-				// 	name: 'Ricardo Alcocer',
-				// 	title: 'Director DevRel & Training'
-				// }, {
-				// 	username: 'FokkeZB',
-				// 	name: 'Fokke Zandbergen',
-				// 	title: 'Developer Evangelist'
 				}, {
-					username: 'JHaynie',
-					name: 'Jeff Haynie',
-					title: 'Co-Founder & CEO'
+					username: 'jasonkneen',
+					name: 'Jason Kneen',
+					title: 'Developer Evangelist'
 				}, {
 					username: 'Hansemannnn',
 					name: 'Hans Knoechel',
 					title: 'iOS Engineer'
-				}, {
-					username: 'grantges',
-					name: 'Bert Grantges',
-					title: 'Titanium PM'
 				}
-				/*, {
-								username: 'Appcelerator',
-								name: 'Appcelerator',
-								title: 'Corporate Account'
-							}*/
 			]
 		}
 	}
